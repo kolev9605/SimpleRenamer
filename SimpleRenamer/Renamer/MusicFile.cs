c413af2cefa358a4a0a1302cd8c245b5ccf36642
kolev9605 <kolev9605@gmail.com>
2017-08-26 04:29:47 +0300

    Update

---
+++ SimpleRenamer/Renamer/MusicFile.cs
@@ -0,0 +1,11 @@
+﻿namespace Renamer
+{
+    public class MusicFile
+    {
+        public string FileName { get; set; }
+
+        public string Directory { get; set; }
+
+        public string FullDirectory { get; set; }
+    }
+}
